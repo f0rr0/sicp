a1b6edce9a630c6fbef43f70d56d0ff3b12c4b43
Sid <sid26@ucla.edu>
2016-08-08 18:32:48 +0530

    lexical scoping

--- sicp-solutions.scm
+++ sicp-solutions.scm
@@ -9,19 +9,15 @@
 
 ;;; Exercise 1.7.  The good-enough? test used in computing square roots will not be very effective for finding the square roots of very small numbers. Also, in real computers, arithmetic operations are almost always performed with limited precision. This makes our test inadequate for very large numbers. Explain these statements, with examples showing how the test fails for small and large numbers. An alternative strategy for implementing good-enough? is to watch how guess changes from one iteration to the next and to stop when the change is a very small fraction of the guess. Design a square-root procedure that uses this kind of end test. Does this work better for small and large numbers?
 
-(define (sqrt-iter guess x)
-  (if (good-enuf? guess x)
-    guess
-    (sqrt-iter (improve guess x) x)))
-
-(define (improve guess x)
-  (average guess (/ x guess)))
-
-(define (average x y)
-  (/ (+ x y) 2))
-
-(define (good-enuf? guess x)
-  (< (abs (- (improve guess x) guess)) (* guess 0.0001)))
-
 (define (sqrt x)
-  (sqrt-iter 1.0 x))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (average a b)
+    (/ (+ a b) 2))
+  (define (good-enuf? guess)
+    (< (abs (- (improve guess) guess)) (* guess 0.0001)))
+  (define (sqrt-iter guess)
+    (if (good-enuf? guess)
+      guess
+      (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
