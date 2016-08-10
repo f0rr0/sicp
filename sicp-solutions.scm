5d524ebe84b4781d4435548d8fe82339a1c22435
Sid <sid26@ucla.edu>
2016-08-10 17:08:46 +0530

    added solutions

--- sicp-solutions.scm
+++ sicp-solutions.scm
@@ -36,3 +36,20 @@
       guess
       (cbrt-iter (improve guess))))
   (cbrt-iter 1.0))
+
+;;; Exercise 1.11.  A function f is defined by the rule that f(n) = n if n<3 and f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n> 3. Write a procedure that computes f by means of a recursive process. Write a procedure that computes f by means of an iterative process.
+
+;;; Recursive process
+(define (f n)
+  (cond ((< n 3) n)
+        (else (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3)))))))
+
+;;; Iterative process
+(define (f n)
+  (define (f-iter a b c x)
+    (if (= x 2)
+      (+ (* a 2) (* b 1) (* c 0))
+      (f-iter (+ a b) (+ (* 2 a ) c) (* 3 a) (- x 1))))
+  (if (< n 3)
+    n
+    (f-iter 1 2 3 (- n 1))))
